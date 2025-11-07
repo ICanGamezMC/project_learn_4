f256f9fdaf92146df4b6806a9287c3e8021883f1
ICanGamez <Trentity2000@gmail.com>
2025-11-07 17:43:53 -0600

    Closes Window Now!

--- pygame_learn_1.py
+++ pygame_learn_1.py
@@ -5,3 +5,26 @@ import math
 pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
+
+
+running = True
+
+
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+
+
+
+
+
+
+
+
+
