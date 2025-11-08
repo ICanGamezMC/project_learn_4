992e0f449d604eee20cff42e34c0a59fbf8e9d47
ICanGamez <Trentity2000@gmail.com>
2025-11-07 18:40:16 -0600

    Learning setting up projection mapping

--- pygame_learn_1.py
+++ pygame_learn_1.py
@@ -10,6 +10,25 @@ screen = pygame.display.set_mode(size)
 running = True
 
 
+def project(point,width,height,scale, viewer_distance):
+    x, y, z = point
+    factor = scale / (z + viewer_distance)
+    X = int(x *factor + width / 2)
+    Y = int(-y *factor + height / 2)
+    return (X, Y)
+
+
+#TEST CUBE POINTS
+points = [
+    [-1, -1, -1],
+    [ 1, -1, -1],
+    [ 1,  1, -1],
+    [-1,  1, -1],
+    [-1, -1,  1],
+    [ 1, -1,  1],
+    [ 1,  1,  1],
+    [-1,  1,  1]
+]
 
 
 while running:
